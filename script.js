67aea8ca9651debd390294a84a479a52254bae67
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2020-12-29 16:26:34 +0300

    fix code

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 
 
 let library = [{ book: 'красная шапочка' },
-            { book: 'колобок' },
-            { book: 'федорино горе' },
-            { book: 'летучий корабль' },
-            { book: 'сказка о царе солтане' },
-            { book: 'муха цокотуха' },
-            { book: 'три богатыря' },
-            { book: 'три поросенка' },
-            { book: 'марья искусница' },
-            { book: 'лиса и журавль' },
+{ book: 'колобок' },
+{ book: 'федорино горе' },
+{ book: 'летучий корабль' },
+{ book: 'сказка о царе солтане' },
+{ book: 'муха цокотуха' },
+{ book: 'три богатыря' },
+{ book: 'три поросенка' },
+{ book: 'марья искусница' },
+{ book: 'лиса и журавль' },
 ];
 
 const proxy = new Proxy(library, {
-    get(target, book) {
-        for( value of target) {
-            if ( value.book === book) {
-                return value.book.split(' ').map(letter => letter[0].toUpperCase() + letter.substring(1)).join(' ')
-            }     
-        }
-        alert('Такой книги НЭМА((') 
-    },
-  
-set(target, prop, value) {
+  get(target, book) {
+    for (value of target) {
+      if (value.book === book) {
+        return value.book.split(' ').map(letter => letter[0].toUpperCase() + letter.substring(1)).join(' ')
+      }
+    }
+    alert('Такой книги НЭМА((')
+  },
+
+  set(target, prop, value) {
     for (val of target) {
       if (value === value.book) {
         alert("Такое уже есть");
@@ -35,5 +35,5 @@ set(target, prop, value) {
 
 console.log(proxy['колобок']);
 proxy.newBook = "морозко";
-console.log(proxy)
+console.log(proxy);
 console.log(proxy['сказка о золотой рыбке'])
